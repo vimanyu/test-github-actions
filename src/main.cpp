b6f4bd3d0147b1b08fd4488a33ad2b2badba616a
Vimanyu <vimanyu87@gmail.com>
2020-06-11 15:54:07 -0700

    adding tinyxml

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "tinyxml.h"
+#include <tinyxml_export.h>
+
 
 int main()
 {
+  TiXmlDocument doc("/tmp/onesphere.xml");
   std::cout << "Yes! Successfully ran EVERYTHING!" << std::endl;
 }
